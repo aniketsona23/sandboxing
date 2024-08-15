ce4b10a7dea7ce5eef9b2d53f04a8b19a5c681a4
Aniket Sonawane <aniket.sonawane1734@gmail.com>
2024-08-16 01:47:00 +0530

    responsiveness

--- src/Components/menuIcon.jsx
+++ src/Components/menuIcon.jsx
@@ -1,18 +1,17 @@
 import React, { useState } from "react";
 import "../styles/menu-icon.css";
 
-export default function MenuIcon({state,changeState}) {
+export default function MenuIcon({ state, changeState }) {
   const handler = () => {
     if (state === "open") {
       changeState("closed");
     } else {
-
       changeState("open");
     }
   };
 
   return (
-    <div className={`menu-icon-${state}`} onClick={handler}>
+    <div className={`menu-icon menu-icon-${state}`} onClick={handler}>
       <div className="line1 line"></div>
       <div className="line2 line"></div>
     </div>
